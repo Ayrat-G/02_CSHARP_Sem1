28d3f3271e562d88b5bf1100f49e37f63ee7e16f
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 21:16:21 +0500

    написал программу, которая на вход принимает два числа и выводит, какое число большее, а какое меньшее, либо сообщает, что оба значения равны между собой

---
+++ HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая на вход принимает два числа и выводит, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1 + " - большее число");
+    Console.WriteLine(num2 + " - меньшее число");
+}
+else
+{
+    if (num1 < num2)
+    {
+        Console.WriteLine(num1 + " - меньшее число");
+        Console.WriteLine(num2 + " - большее число");
+    }
+    else
+    {
+        Console.WriteLine("Оба числа между собой равны");
+    }
+}
