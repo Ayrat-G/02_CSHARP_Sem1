4a920d5c2304b696e64245aefc3eb49ccd72d8c3
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 17:57:04 +0500

    Задачка по проверке, является ли первое число квадартом второго, с вводом пользовательских данных

--- Sem1_Task1/Program.cs
+++ Sem1_Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая на вход принимает два целых числа, и проверяет, является ли первое число квадратом второго */
+
+Console.WriteLine("Введите первое число: ");
+String num1Str = Console.ReadLine();
+int num1 = Convert.ToInt32(num1Str);
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = 25;
+// int num2 = 5;
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
