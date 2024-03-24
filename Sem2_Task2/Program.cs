a16db50a6f1d6f85fca00ab2ff9ae6c559efb08f
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 23:15:23 +0500

    добавил возможность производить расчеты при вводе отрицательного трехзначного числа

--- Sem2_Task2/Program.cs
+++ Sem2_Task2/Program.cs
@@ -4,17 +4,25 @@ Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int count = 0;
-int result = 1;
+double result = 1;
 
 if (num >= 100 && num <= 999 || num <= -100 && num >= -999)
 {
     int digit2 = num / 10 % 10;
     int digit3 = num % 10;
-
+   
+    if (digit3 < 0)
+    {
+        digit3 *= -1; // digit3 = digit3 * -1
+    }
     while (count < digit3) // Почему <, а не <=, потому что count начинаем с нуля, а не с 1
     {
         result = result * digit2;
         count++;
+    }
+    if (digit2 < 0)
+    {
+        result = 1 / result;
     }
     Console.Write(result);
 }
