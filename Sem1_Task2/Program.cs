3628ce57289c8bae5e48bf96150dd9c87aeb3c14
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 21:34:31 +0500

    добавил в задачу 1 домашнего задания функцию сравнения и вывода на печать, когда оба значения равны

--- Sem1_Task2/Program.cs
+++ Sem1_Task2/Program.cs
@@ -3,7 +3,7 @@
 Console.WriteLine("Введите целое положительное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 0)
+if(num > 0) 
 {
     // Задаем начальное значение отсчета (перебора значений)
     int count = -num;
