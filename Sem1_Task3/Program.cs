da20ba2e4863b5ea3be7905fadf943db8b79bbc3
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 20:01:05 +0500

    Написал программу, которая принимает на вход трехзначное целое число, в том чсиле отрицательное, и на выходе показывает сумму первой и последней цифры этого числа

---
+++ Sem1_Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трехзначное целое число и на выходе показывает сумму первой и последней цифры этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = num * -1;
+}
+
+    if (num >= 100 && num <= 999)
+    {
+        int digit1 = num / 100;
+        int digit3 = num % 10;
+        int result = digit1 + digit3;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+    }
