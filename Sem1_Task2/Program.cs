946de129b0ea2d7d5de9290bdf48d72855b7e12d
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 19:06:00 +0500

    Добавили проверку вводимого числа на положительное либо отрицательное значение

--- Sem1_Task2/Program.cs
+++ Sem1_Task2/Program.cs
@@ -3,12 +3,19 @@
 Console.WriteLine("Введите целое положительное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-// Задаем начальное значение отсчета (перебора значений)
-int count = -num;
-
-// Циклом while перебираем все значения с начальной точки -num до num
-while (count <= num)
+if (num > 0)
 {
-    Console.Write(count + " "); // Печать значений в строчку с пробелом
-    count++;    // Это означает count = count + 1
+    // Задаем начальное значение отсчета (перебора значений)
+    int count = -num;
+
+    // Циклом while перебираем все значения с начальной точки -num до num
+    while (count <= num)
+    {
+        Console.Write(count + " "); // Печать значений в строчку с пробелом
+        count++;    // Это означает count = count + 1
+    }    
+}
+else
+{
+        Console.WriteLine("Некорректный ввод!");
 }
