65dfd3e6dc2569a2aa77070972c30d2071eaf3fa
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 22:54:01 +0500

    Программа, которая принимает на вход трехзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре

---
+++ Sem2_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принимает на вход трехзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int result = 1;
+
+if (num >= 100 && num <= 999 || num <= -100 && num >= -999)
+{
+    int digit2 = num / 10 % 10;
+    int digit3 = num % 10;
+
+    while (count < digit3) // Почему <, а не <=, потому что count начинаем с нуля, а не с 1
+    {
+        result = result * digit2;
+        count++;
+    }
+    Console.Write(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
